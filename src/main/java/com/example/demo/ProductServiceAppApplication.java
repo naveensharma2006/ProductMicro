db04f5742577f439fb2bf02c20d0bb889db8efa0
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-09-02 13:06:58 +0530

    Initial COmmit

---
+++ src/main/java/com/example/demo/ProductServiceAppApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ *
+ */
+@SpringBootApplication
+public class ProductServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceAppApplication.class, args);
+	}
+
+}
